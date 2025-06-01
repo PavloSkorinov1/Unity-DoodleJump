6be272a7391359a3e1e73d4d258572a412ff06a9
PavloSkorinov <pavlo.skorinov@gmail.com>
2025-06-01 12:05:24 +0300

    Finished HW 8 - added Player script, movement, jump and run features

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -1,44 +1,94 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Character
 {
-
-    [SerializeField] private Rigidbody2D _rb;
-    private bool _isJump = false;
-    [SerializeField] private float _jumpForce = 3;
-    private bool _isGrounded = false;
-    [SerializeField] private float _groundCheckDistance = 0.2f;
-    [SerializeField] private LayerMask _groundMask;
-    
-    void Start()
+    public class Player : MonoBehaviour
     {
-        _groundMask = LayerMask.GetMask("Ground");
-    }
-    
-    void Update()
-    {
-        CalculateJump();
 
-    }
+        private Rigidbody2D _rb;
+        private Animator _anim;
+        private bool _isJump = false;
+        private float _horizontalInput;
+        
+        [Header("Jump Settings")]
+        [SerializeField] private float jumpForce = 3;
+        [SerializeField] private float groundCheckDistance = 0.2f;
+        [SerializeField] private LayerMask groundMask;
+        [SerializeField] private Transform groundCheck;
+        
+        [Header("Movement Settings")]
+        [SerializeField] private float moveSpeed = 5f;
 
-    private void CalculateJump()
-    {
-        _isGrounded = Physics2D.Raycast(_rb.position, Vector2.down,_groundCheckDistance, _groundMask);
-        if (Input.GetKeyDown(KeyCode.W))
+        void Start()
         {
-            _isJump = true;
+            groundMask = LayerMask.GetMask("Ground");
+            _rb = GetComponent<Rigidbody2D>();
+            _anim = GetComponent<Animator>();
         }
-    }
 
-    private void FixedUpdate()
-    {
-        if (_isGrounded)
+        void Update()
         {
-            if (_isJump)
+            CalculateInput();
+        }
+
+        private void FixedUpdate()
+        {
+            CalculateMove();
+            CalculateJump();
+            CalculateAnimation();
+        }
+
+        private void CalculateInput()
+        {
+            _horizontalInput = Input.GetAxis("Horizontal");
+            if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
-                _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+                _isJump = true; 
+            }
+        }
+
+        private void CalculateMove()
+        {
+            
+            Vector2 movement = new Vector2(_horizontalInput * moveSpeed, _rb.linearVelocity.y);
+            _rb.linearVelocity = movement;
+        }
+
+        private void CalculateJump()
+        {
+            if (_isJump && IsGrounded())
+            {                
+                _rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
                 _isJump = false;
-            }  
+            }
+            else if (_isJump)
+            {
+                _isJump = false;
+            }
+        }
+
+        private void CalculateAnimation()
+        {
+            _anim.SetFloat("Speed", Mathf.Abs(_horizontalInput));
+
+            if (_horizontalInput > 0)
+            {
+                transform.localScale = new Vector3(3, 3, 1);
+            }
+            else if (_horizontalInput < 0)
+            {
+                transform.localScale = new Vector3(-3, 3, 1);
+            }
+            
+            _anim.SetBool("IsGrounded", IsGrounded());
+            _anim.SetFloat("VerticalSpeed", _rb.linearVelocity.y);
+        }
+        
+        private bool IsGrounded()
+        {
+            if (groundCheck == null) return false;
+            RaycastHit2D hit = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundMask);
+            return hit.collider != null;
         }
     }
 }
